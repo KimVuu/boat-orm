3b6682f3e6ca5e933f3af7a85de0ef49c42bdd58
KimVuu <rmfoarmfoa@naver.com>
2021-12-23 22:39:46 +0900

    feat: utils dict.py > model.py 통합

--- boat/utils/model.py
+++ boat/utils/model.py
@@ -1,12 +1,24 @@
-from boat.utils.dict import FieldDictDecorator
+from boat.utils.field import Field
 
 
-__all__ = ('Model',)
+__all__ = ('FieldDict', 'FieldDictDecorator', 'Model',)
+
+
+class FieldDict:
+    def __init__(self, table_name: str = ''):
+        self._table_name = table_name
+
+    def __getitem__(self, item: str) -> Field:
+        return Field(item, table_name=self._table_name)
+
+
+class FieldDictDecorator(property):
+    def __get__(self, *args, **kwargs):
+        owner: "Model" = args[1]
+        return FieldDict(owner.__table_name__)
 
 
 class Model:
     __table_name__: str
 
-    @FieldDictDecorator
-    def fields(cls):
-        pass
+    fields: FieldDict = FieldDictDecorator()
