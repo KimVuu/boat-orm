31a666ba8a6189479739b67c2ab53b45652b8819
KimVuu <rmfoarmfoa@naver.com>
2021-12-30 15:59:00 +0900

    feat: field, dao class > model.py로 통합

--- boat/utils/model.py
+++ boat/utils/model.py
@@ -1,7 +1,58 @@
-from boat.utils.field import Field
+from typing import Tuple, TypeVar, Generic, Sequence, Optional, Any
+
+from orator import DatabaseManager
+from orator.query.builder import QueryBuilder
 
 
-__all__ = ('FieldDict', 'FieldDictDecorator', 'Model',)
+__all__ = ('WhereClause', 'Field', 'FieldDict', 'FieldDictDecorator', 'Model', 'DAO',)
+
+
+T = TypeVar('T')
+
+
+class WhereClause:
+    def __init__(self, where_type: str, field: str, operator: Optional[Any] = None, value: Optional[Any] = None):
+        self.where_type: str = where_type
+        self.field: str = field
+        self.operator: Optional[Any] = operator
+        self.value: Optional[Any] = value
+
+    def get(self) -> Tuple[str, str, Optional[Any], Optional[Any]]:
+        return self.where_type, self.field, self.operator, self.value
+
+    def filter(self) -> Tuple[str, ...]:
+        return tuple(filter(lambda x: x, self.get()))
+
+    def where(self) -> Tuple[str, ...]:
+        return self.filter()
+
+
+class Field(Generic[T]):
+    def __init__(self, field_name: str, table_name: str = ''):
+        self._field_name: str = field_name
+        self._table_name: str = table_name
+
+    def get_field_name(self) -> str:
+        if self._table_name:
+            return f"{self._table_name}.{self._field_name}"
+        return self._field_name
+
+    def asc(self) -> Tuple[str, str]: return self.get_field_name(), "ASC"
+    def desc(self) -> Tuple[str, str]: return self.get_field_name(), "DESC"
+
+    def eq(self, value: T) -> WhereClause: return WhereClause("where", self.get_field_name(), "=", value)
+    def gt(self, value: T) -> WhereClause: return WhereClause("where", self.get_field_name(), ">", value)
+    def gte(self, value: T) -> WhereClause: return WhereClause("where", self.get_field_name(), ">=", value)
+    def lt(self, value: T) -> WhereClause: return WhereClause("where", self.get_field_name(), "<", value)
+    def lte(self, value: T) -> WhereClause: return WhereClause("where", self.get_field_name(), "<=", value)
+
+    def in_(self, values: Sequence[T]) -> WhereClause: return WhereClause("where_in", self.get_field_name(), values)
+
+    def not_in(self, values: Sequence[T]) -> WhereClause:
+        return WhereClause("where_not_in", self.get_field_name(), values)
+
+    def is_null(self) -> WhereClause: return WhereClause("where_null", self.get_field_name())
+    def is_not_null(self) -> WhereClause: return WhereClause("where_not_null", self.get_field_name())
 
 
 class FieldDict:
@@ -13,7 +64,7 @@ class FieldDict:
 
 
 class FieldDictDecorator(property):
-    def __get__(self, *args, **kwargs):
+    def __get__(self, *args, **kwargs) -> FieldDict:
         owner: "Model" = args[1]
         return FieldDict(owner.__table_name__)
 
@@ -22,3 +73,18 @@ class Model:
     __table_name__: str
 
     fields: FieldDict = FieldDictDecorator()
+
+
+class DAO:
+    __model__: Model
+
+    def __init__(self, *args):
+        self.args = args
+
+    def __call__(self, **kwargs) -> QueryBuilder:
+        database: DatabaseManager = kwargs.pop('db')
+        builder: QueryBuilder = database.table(self.__model__.__table_name__)
+        for sql_func in self.args:
+            if sql_func is not None:
+                sql_func(builder)
+        return builder
