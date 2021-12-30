2abff28a188bace600b24f2f0bcf2c819828d8c4
KimVuu <rmfoarmfoa@naver.com>
2021-12-30 16:03:36 +0900

    feat: install package 모듈 수정 및 분리 최적화

--- setup.py
+++ setup.py
@@ -1,4 +1,18 @@
 from setuptools import setup
 
+install_requires = [
+    "orator==0.9.9",
+]
 
-setup()
+testing_requires = [
+    "flake8==4.0.1",
+    "isort==5.10.1",
+    "mypy==0.930",
+]
+
+setup(
+    install_requires=install_requires,
+    extras_require={
+        'test': testing_requires,
+    }
+)
